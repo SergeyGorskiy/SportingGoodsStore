4ee5be7aa24cd662c28984065017eb798bb06564
Sergey Nazhestkin <9497020@mail.ru>
2021-11-09 21:44:07 +0300

    Разработана структура MVC.

---
+++ SportingGoodsStore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SportingGoodsStore.Models;
+
+namespace SportingGoodsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+
+                results.Add(string.Format($"Name: {name}, Price: {price}, Related: {relatedName}"));
+            }
+            return View(results);
+        }
+    }
+}
