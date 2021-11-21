7741745fc6371c91120654ade184cdfa5bce3eb1
Sergey Nazhestkin <9497020@mail.ru>
2021-11-21 22:45:56 +0300

    Покрыты тестами методы логики корзины. Разработана визуализация корзины.

--- SportingGoodsStore/Pages/Cart.cshtml.cs
+++ SportingGoodsStore/Pages/Cart.cshtml.cs
@@ -1,16 +1,38 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportingGoodsStore.EF;
+using SportingGoodsStore.Infrastructure;
+using SportingGoodsStore.Models;
 
 namespace SportingGoodsStore.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private readonly IStoreRepository _repository;
+
+        public CartModel(IStoreRepository repository)
         {
+            _repository = repository;
+        }
+
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
