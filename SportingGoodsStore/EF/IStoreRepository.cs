a5205381ed9fca28fa6122b340e26ea11b7738aa
Sergey Nazhestkin <9497020@mail.ru>
2021-11-26 11:38:06 +0300

    Начата реализация функционала администрирования товаров.

--- SportingGoodsStore/EF/IStoreRepository.cs
+++ SportingGoodsStore/EF/IStoreRepository.cs
@@ -6,5 +6,11 @@ namespace SportingGoodsStore.EF
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        void CreateProduct(Product product);
+
+        void DeleteProduct(Product product);
     }
 }
--- SportingGoodsStore/EF/StoreRepository.cs
+++ SportingGoodsStore/EF/StoreRepository.cs
@@ -13,5 +13,22 @@ namespace SportingGoodsStore.EF
         }
 
         public IQueryable<Product> Products => _context.Products;
+        
+        public void CreateProduct(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _context.Remove(product);
+            _context.SaveChanges();
+        }
+
+        public void SaveProduct(Product product)
+        {
+            _context.SaveChanges();
+        }
     }
 }
